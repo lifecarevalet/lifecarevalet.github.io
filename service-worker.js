6ee0be2ea1b957714e84982b3863417fbc6fc945
lifecarevalet <lifecarevalet@gmail.com>
2025-12-10 17:41:59 +0530

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,12 @@
+{
+  "name": "Life Care Valet Parking",
+  "short_name": "Life Valet",
+  "start_url": "/",
+  "display": "standalone",
+  "background_color": "#1466f2",
+  "theme_color": "#1466f2",
+  "icons": [
+    { "src": "icons/icon-192.png", "sizes":"192x192", "type":"image/png" },
+    { "src": "icons/icon-512.png", "sizes":"512x512", "type":"image/png" }
+  ]
+}
