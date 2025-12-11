f48af165485bcec9c73e6b41c3dd78c6c020d577
lifecarevalet <lifecarevalet@gmail.com>
2025-12-11 14:46:14 +0530

    Delete vite.config.js

--- vite.config.js
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  // ✅ FIX 1: GitHub Pages ke liye base path ko './' par set kiya
-  base: './', 
-  plugins: [react()],
-})
