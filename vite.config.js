6b83f0ea34d5b3bc3ec3a338993a354764ea78b1
lifecarevalet <lifecarevalet@gmail.com>
2025-12-11 13:42:36 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  // ✅ GitHub Pages ke liye base path '/' set kiya gaya hai
-  base: '/', 
+  // ✅ FIX 1: GitHub Pages ke liye base path ko './' par set kiya
+  base: './', 
   plugins: [react()],
 })
