5c358cdc4eae07833ede210bd1689a61cd93bdcf
lifecarevalet <lifecarevalet@gmail.com>
2025-12-11 11:58:09 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // --- YEH LINE ADD KARENGE ---
+  // GitHub Pages ke liye base path '/' set kiya gaya hai
   base: '/', 
-  // -----------------------------
   plugins: [react()],
 })
