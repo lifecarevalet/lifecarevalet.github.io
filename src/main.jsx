dc9c060fadc62ecf0c0d807d27349e0e076ca3f1
lifecarevalet <lifecarevalet@gmail.com>
2025-12-11 13:00:23 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './app.jsx'; // <--- App component ko import kiya
-import './index.css'; // <--- Tailwind CSS directives ko load kiya
+// -------------------------------------------------------------
+// YAHAN BADALNA HAI: Filename ko exact match karein: app.js
+import App from './app.js'; 
+// -------------------------------------------------------------
+import './index.css'; 
 
-// Window mein React App ko '#root' div ke andar daalo
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
