7a7ef38c6e6d1552e0c744d67565793972ddc551
lifecarevalet <lifecarevalet@gmail.com>
2025-12-11 14:45:29 +0530

    Delete src directory

--- src/config.js
+++
@@ -1,4 +0,0 @@
-// src/config.js
-
-// API_BASE_URL ko aapke live Railway backend URL se update kiya gaya hai.
-export const API_BASE_URL = 'https://lifecarevaletbackend-production.up.railway.app/api'; 
